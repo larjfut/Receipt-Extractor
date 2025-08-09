6fcf0199bb31ff4d8e46f9d888509a2065ddadce
larjfut <larjfut@gmail.com>
2025-08-09 13:08:57 -0500

    test: configure jest for import.meta

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js'
   }
 }
